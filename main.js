3a1966ad8659ca27ec60db290ed1c20af6e84bcf
Euzarsiph <shafiqrehman444@gmail.com>
2025-12-04 02:38:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true
+        },
+        icon: path.join(__dirname, 'icon.png') // Placeholder, icon might not exist yet
+    });
+
+    win.loadFile('index.html');
+
+    // Remove menu bar for a cleaner look
+    win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
