9fcf74ebec2230719f6b8ab85b25c0c2212fc349
Euzarsiph <shafiqrehman444@gmail.com>
2025-12-04 03:52:15 -0700

    Update sw.js to v1.1 for cache busting and full 500 cards

--- sw.js
+++ sw.js
@@ -1,25 +1,56 @@
-const CACHE_NAME = 'ira-flashcards-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './css/style.css',
-    './js/app.js',
-    './js/flashcards.js',
-    './data/flashcards.json',
-    './icon.svg',
-    './manifest.json'
+const CACHE_NAME = 'ira-flashcards-v1.1'; // Updated version to clear old cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg',
+  '/main.js',
+  '/css/style.css',
+  '/data/flashcards.json', // Now pulls the full 500
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            let responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// Clear old caches on activation
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
